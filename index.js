1f66fae60f4817e5ad2ded31047194c3bfeef3b1
kyo <a@kyo5884.tk>
2020-01-18 21:35:55 +0900

    adjust cursorColor and borderColor

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
 
 exports.decorateConfig = (config) => {
   return Object.assign(config, {
+    cursorColor: 'rgba(255, 255, 255, 0.5)',
     backgroundColor: 'rgba(0, 0, 0, 0)',
+    borderColor: 'rgba(128, 128, 128, 0.4)',
     css: `
       ${config.css || ''}
       div.term_fit {
