9896b41c189d87a96e8a34313c71b56c12a8938b
kyo <a@kyo5884.tk>
2020-01-18 21:48:02 +0900

    adjust selectionColor

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
+exports.onWindow = browserWindow => browserWindow.setVibrancy('ultra-dark');
 
 exports.decorateConfig = (config) => {
   return Object.assign(config, {
     cursorColor: 'rgba(255, 255, 255, 0.5)',
+    selectionColor: 'rgba(255, 128, 128, 0.4)',
     backgroundColor: 'rgba(0, 0, 0, 0)',
     borderColor: 'rgba(128, 128, 128, 0.4)',
     css: `
